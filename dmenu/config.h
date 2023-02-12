a1d8d4bd888d844b895ee26a0fc1e36de4f4b2bd
Arsemy <arsemy2@gmail.com>
2023-02-12 07:45:11 -0300

    tweak(dmenu): remove one line

--- dmenu/config.h
+++ dmenu/config.h
@@ -16,7 +16,7 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 13;
+static unsigned int lines      = 12;
 /* -h option; minimum height of a menu line */
 static unsigned int lineheight = 30;
 static unsigned int min_lineheight = 8;
