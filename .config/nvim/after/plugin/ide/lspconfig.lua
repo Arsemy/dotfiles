b5ca672fa205216af185ba886604f09e8f9a9255
Arsemy <arsemy2@gmail.com>
2023-02-13 14:43:47 -0300

    refactor(nvim): remove comment about lsp globals

--- .config/nvim/after/plugin/ide/lspconfig.lua
+++ .config/nvim/after/plugin/ide/lspconfig.lua
@@ -64,7 +64,6 @@ require('lspconfig').lua_ls.setup({
   settings = {
     Lua = {
       diagnostics = {
-        -- Get the language server to recognize the `vim` global.
         globals = {'vim'},
       },
     },
