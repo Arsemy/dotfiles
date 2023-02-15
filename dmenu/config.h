bab9ccd095e7e54b215b5ff8578521b9cc0a0c79
Arsemy <arsemy2@gmail.com>
2023-02-15 11:06:46 -0300

    tweak(dmenu): remove 1 line, increase line height, decrease window width

--- dmenu/config.h
+++ dmenu/config.h
@@ -3,7 +3,7 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int centered = 1;                    /* -c option; centers dmenu on screen */
-static int min_width = 500;                    /* minimum width when centered */
+static int min_width = 470;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"cascadia code:size=10"
@@ -16,9 +16,9 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 12;
+static unsigned int lines      = 11;
 /* -h option; minimum height of a menu line */
-static unsigned int lineheight = 30;
+static unsigned int lineheight = 31;
 static unsigned int min_lineheight = 8;
 
 /*
