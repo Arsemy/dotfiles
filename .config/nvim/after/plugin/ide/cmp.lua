5e260de9533b8086785a2ff8bc9aea26cb4fef80
Arsemy <arsemy2@gmail.com>
2023-02-16 11:02:30 -0300

    tweak(nvim): confirm cmp completion with <c-/>

--- .config/nvim/after/plugin/ide/cmp.lua
+++ .config/nvim/after/plugin/ide/cmp.lua
@@ -33,7 +33,7 @@ cmp.setup({
     ['<c-f>'] = cmp.mapping.scroll_docs(4),
     ['<c-e>'] = cmp.mapping.abort(),
     ['<nop>'] = cmp.mapping.complete(),
-    ['<tab>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ['<c-/>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
